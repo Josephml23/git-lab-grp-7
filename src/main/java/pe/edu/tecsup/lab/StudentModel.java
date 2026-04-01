38ead063950953e847c9ab37190e4cd716404ec9
Cesar <cesar.kana@tecsup.edu.pe>
2026-03-31 22:22:55 -0500

    feat: se agrega StudentModel

---
+++ src/main/java/pe/edu/tecsup/lab/StudentModel.java
@@ -0,0 +1,40 @@
+package pe.edu.tecsup.lab;
+
+public class StudentModel {
+    private int id;
+    private String nombre;
+    private String apellido;
+
+    public StudentModel() {
+    }
+
+    public StudentModel(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
